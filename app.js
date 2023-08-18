2d9b4e9ffaba048ce322d33605edb156ebba75e1
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-18 11:51:44 -0600

    save at noon

--- app.js
+++ app.js
@@ -1,83 +1,113 @@
-import express from 'express';
-import session from 'express-session';
-import lodash from 'lodash';
-import morgan from 'morgan';
-import nunjucks from 'nunjucks';
-import ViteExpress from 'vite-express';
+import express from "express";
+import session from "express-session";
+import lodash from "lodash";
+import morgan from "morgan";
+import nunjucks from "nunjucks";
+import ViteExpress from "vite-express";
 
 const app = express();
-const port = '8000';
+const port = "8000";
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+app.use(express.static("public"));
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
 const MOST_LIKED_FOSSILS = {
   aust: {
-    img: '/img/australopith.png',
-    name: 'Australopithecus',
+    img: "/img/australopith.png",
+    name: "Australopithecus",
     num_likes: 584,
   },
   quetz: {
-    img: '/img/quetzal_torso.png',
-    name: 'Quetzal',
+    img: "/img/quetzal_torso.png",
+    name: "Quetzal",
     num_likes: 587,
   },
   steg: {
-    img: '/img/stego_skull.png',
-    name: 'Stegosaurus',
+    img: "/img/stego_skull.png",
+    name: "Stegosaurus",
     num_likes: 598,
   },
   trex: {
-    img: '/img/trex_skull.png',
-    name: 'Tyrannosaurus Rex',
+    img: "/img/trex_skull.png",
+    name: "Tyrannosaurus Rex",
     num_likes: 601,
   },
 };
 
 const OTHER_FOSSILS = [
   {
-    img: '/img/ammonite.png',
-    name: 'Ammonite',
+    img: "/img/ammonite.png",
+    name: "Ammonite",
   },
   {
-    img: '/img/mammoth_skull.png',
-    name: 'Mammoth',
+    img: "/img/mammoth_skull.png",
+    name: "Mammoth",
   },
   {
-    img: '/img/ophthalmo_skull.png',
-    name: 'Opthalmosaurus',
+    img: "/img/ophthalmo_skull.png",
+    name: "Opthalmosaurus",
   },
   {
-    img: '/img/tricera_skull.png',
-    name: 'Triceratops',
+    img: "/img/tricera_skull.png",
+    name: "Triceratops",
   },
 ];
 
-app.get('/', (req, res) => {
-  res.render('homepage.html.njk');
+app.get("/", (req, res) => {
+  if (req.session.userName) {
+    res.redirect("/top-fossils");
+  } else {
+    res.render("homepage.html.njk");
+  }
 });
 
-app.get('/top-fossils', (req, res) => {
-  const userName = req.session.userName || 'Guest';
-  res.render('top-fossils.html.njk', { fossils: MOST_LIKED_FOSSILS, userName });
+app.get("/top-fossils", (req, res) => {
+  if (req.session.userName) {
+    res.render("top-fossils.html.njk", {
+      fossils: MOST_LIKED_FOSSILS,
+      userName: req.session.userName,
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
-app.get('/get-name', (req, res) => {
+app.get("/get-name", (req, res) => {
   const { name } = req.query;
   req.session.userName = name;
-  res.redirect('/top-fossils');
+  res.redirect("/top-fossils");
 });
 
 app.get('/random-fossil.json', (req, res) => {
-  const randomFossil = lodash.sample(OTHER_FOSSILS);
+  const randomIndex = Math.floor(Math.random() * OTHER_FOSSILS.length);
+  const randomFossil = OTHER_FOSSILS[randomIndex];
   res.json(randomFossil);
+});
+
+
+app.post("/like-fossil", (req, res) => {
+  const { fossilId } = req.body;
+
+  if (MOST_LIKED_FOSSILS.hasOwnProperty(fossilId)) {
+    MOST_LIKED_FOSSILS[fossilId].num_likes++;
+
+    const userName = req.session.userName || "Guest";
+    const userFullName =
+      userName === "Guest" ? "Guest" : "Thank you, " + userName;
+
+    res.render("thank-you.html.njk", { userFullName });
+  } else {
+    res.redirect("/top-fossils");
+  }
 });
 
 ViteExpress.listen(app, port, () => {
--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import axios from 'axios';
+
+const getRandomFossilBtn = document.getElementById('getRandomFossilBtn');
+
+getRandomFossilBtn.addEventListener('click', async () => {
+  try {
+    const response = await axios.get('/random-fossil.json');
+    const randomFossil = response.data;
+
+    const randomFossilImage = document.getElementById('random-fossil-image');
+    randomFossilImage.innerHTML = `<img src="${randomFossil.img}" alt="${randomFossil.name}" />`;
+
+    const randomFossilName = document.getElementById('random-fossil-name');
+    randomFossilName.textContent = randomFossil.name;
+  } catch (error) {
+    console.error('Error fetching random fossil:', error);
+  }
+});
